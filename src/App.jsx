edfdc2630dfb2e2d1f0b0e375fa2effaffb81de6
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-01-16 15:41:31 -0500

    Updated App.jsx with imports and Routes to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
+// Components
+import Header from "./components/common/Header";
+import ClassList from "./components/ClassList/ClassList";
+import StudentCard from "./components/student/StudentCard";
+import StudentList from "./components/student/StudentList";
+
+// Data 
+import StudentData from "./data/data.json";
 
 function App() {
   return (
-    <div>
-      <h1>Student Dashboard</h1>
+    <div className="wrapper">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Header/>} />
+        <Route path="/ClassList" element={<ClassList StudentData = { StudentData }/>} />
+        <Route path="/StudentList" element={<StudentList StudentData = { StudentData }/>} />
+      </Routes>
     </div>
   );
 }
--- src/components/ClassList/ClassList.jsx
+++ src/components/ClassList/ClassList.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import "./ClassList.css";
+import { Link } from "react-router-dom";
 
 function ClassList() {
   return (

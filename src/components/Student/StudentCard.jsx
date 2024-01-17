8c35385fee8768a66e9ef52a18ff4a80ef886766
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-01-17 14:34:52 -0500

    Updated StudentCard.jsx with data needs more work

--- src/components/Student/StudentCard.jsx
+++ src/components/Student/StudentCard.jsx
@@ -2,15 +2,46 @@ import { useState, useEffect } from "react"
 import { Link, useParams, useNavigate } from "react-router-dom"
 
 import { getOneStudent } from "../../api/fetch"
+import StudentList from "./StudentList"
+
+import ErrorMessage from "../Errors/ErrorMessage"
 
 import "./StudentCard.css"
 
 function StudentCard() {
+  const { id } = useParams();
+  const navigate = useNavigate();
 
+  const [student, setStudent] = useState({});
+  const [loadingError, setLoadingError] = useState(false);
 
+  const {
+    id:studentId,
+    codewars,
+    certifications,
+    cohort
+  } = student;
+
+  useEffect(() => {
+    getOneStudent(id)
+      .then((data) => {
+        setStudent(data);
+        if (Object.keys(data).length === 0) {
+          setLoadingError(true);
+        } else {
+          setLoadingError(false);
+        }
+      })
+      .catch((error) => {
+        setLoadingError(true);
+      });
+  }, [id]);
 
   return (
-    <div>StudentCard</div>
+    <div>
+      <StudentList/>
+      StudentCard
+    </div>
   )
 }
 

03f6a2cbc2e6cf6acecbf6a47c2d9e45131641ac
Isiah Arrufat <isiaharrufat@pursuit.org>
2024-01-18 12:47:09 -0500

    created a co-hort list started ascending and decending button

--- src/components/ClassList/ClassList.jsx
+++ src/components/ClassList/ClassList.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
 import "./ClassList.css";
 import { Link } from "react-router-dom";
+import { useState } from 'react';
+
+const [filter, setFilter] = useState("")
+
+const cohortFilter = ({allStudentList,}) => {
+
+  const filteredCohorts = allStudentList.filter((student) =>  student.cohort.cohortCode === filter)
+
+    return filteredCohorts;
+}
 
 function ClassList() {
 
+   const namesofCohorts2026 = [
+    "Winter 2026",
+    "Spring 2026",
+    "Summer 2026",
+    "Fall 2026",
+
+   ]
+
+   const namesofCohorts2025 = [
+    "Winter 2025",
+    "Spring 2025",
+    "Summer 2025",
+    "Fall 2025",
+   ]
+
+   const [fullClasses, setFullClasses] = useState([ namesofCohorts2025,  namesofCohorts2026 ])
+
+
+  function filterSpaces(inputString) {
+    return inputString.split('').filter(char => char !== ' ').join('');
+  }
+
+  const [button, setButton] = useState("Ascending")
+
+const handleChange = () => {
+  button === "Ascending" ? setButton("Desending") : setButton("Ascending");
+  button ===  "Ascending" ? setFullClasses ([...namesofCohorts2025, ...namesofCohorts2026]): setFullClasses([...namesofCohorts2026, ...namesofCohorts2025])
+}
+
   
   return (
-    <div>ClassList</div>
-  )
+    <div>
+     <h2> Choose A Class By Start Date </h2>
+      <button onClick={handleChange()}> Sort {button} By Year </button>
+      {
+        
+      }
+   </div>
+   )
 }
 
 export default ClassList

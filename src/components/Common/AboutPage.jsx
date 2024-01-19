d4e3be1f5b90bdd5afd3870bba82ed4c4789f5a5
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-01-19 15:24:21 -0500

    Updated names of img and path

--- src/components/Common/AboutPage.jsx
+++ src/components/Common/AboutPage.jsx
@@ -9,13 +9,13 @@ function AboutPage() {
       </h1>
       <div>
         <h2>Michael Restrepoross</h2>
-        <img src="src/img/MichaelRestrepoross.jpg" alt="Michael Restrepoross's picture" /><br />
+        <img src="/img/MichaelRestrepoross.jpg" alt="Michael Restrepoross's picture" /><br />
         <p>Hello, I'm Michael Restrepoross, a 25 year old Full Stack Forward Engineer with a strong foundation in computer science. My fascination with website and video game development from a young age drove me to pursue this field. With a background in cybersecurity, I bring a deep understanding of the importance of secure coding to ensure robust and reliable solutions. I'm now seeking an opportunity to bring my passion and expertise to a dynamic team, where I can contribute to meaningful projects and continue learning. I'm particularly drawn to companies that share my commitment to innovation and creativity. I believe that my proficiency in JavaScript, along with my cybersecurity background, allows me to approach coding with security in mind.</p>
         <a href="https://github.com/MichaelARestrepoross" target='_blank'>GitHub Link</a>
       </div>
       <div>
       <h2>Chris Miranda</h2>
-        <img src="src/img/Christopher Miranda.jpg" alt="Chris Miranda's picture" /><br />
+        <img src="/img/ChristopherMiranda.jpg" alt="Chris Miranda's picture" /><br />
         <p> 
         Hello! My name is Christopher Miranda and I am a 27 year old from Yonkers, NY. I got into coding as it was always an interest of mine, but I never took any action on it. As a kid, I had been the <i>troubleshooter</i> in the house for anything computer/internet related. After not finishing college <small>(as I didn't know what I wanted to major in)</small> I worked dead end jobs for a couple of years. I was then introduced to <strong>Pursuit</strong> from a friend of mine and figured it was the best course of action to be where I want in the future.
         </p>
@@ -23,9 +23,9 @@ function AboutPage() {
       </div>
       <div>
       <h2>Isiah Arrufat</h2>
-        <img src="src/img/isiah_img.jpeg" alt="Isiah Arrufat's picture" /><br />
+        <img src="/img/IsiahArrufat.jpeg" alt="Isiah Arrufat's picture" /><br />
         <p>Description</p>
-        <a href="https://github.com/isiahArrufat" target='_blank'>GitHub Link</a>
+        <a href="https://github.com/IsiahArrufat" target='_blank'>GitHub Link</a>
       </div>
     </div>
   )
